e6216525b99224f56bbceab359128896444e33d1
llygwen <mhughes@uw.edu>
2016-08-23 16:48:01 -0700

    Existing Files from Learn Python the Hard Way
    Files I created previously while using Learn Python the Hard Way

---
+++ ex03.py
@@ -0,0 +1,25 @@
+print "I will now count my chickens:"
+#Math follows order of operations: 
+#Parentheses Exponents Multiplication Division Addition Subtraction. 
+#Divides 30 by 6 giving 5, then adds 25 to that
+print "Hens", 25 + 30 / 6
+#Multiply 25 by 3, giving 75, then divide that by 4 giving 18 
+#with a remainder of 3. Subtract that remainder from 100.
+print "Roosters", 100 - 25 * 3 % 4
+print "Now I will count the eggs:"
+#Divide 4 by 2 and take the remainder (0). Divide 1 by 4 giving 0 (integer).
+#Add 3 + 2 -> (5) +1 -> (6) -5 -> (1) +0 -> (1) -0 -> (1) +6 -> 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "Is it true that 3 + 2 < 5 - 7?"
+# Is (5) less than (-2)
+print 3 + 2 < 5 - 7
+print "What is 3 + 2", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+#Evaluate truthiness of 5 being greater than -2
+print "Is it greater?", 5 > -2
+#Evaluate truthiness of 5 being greater than or equal to -2
+print "Is it greater or equal?", 5 >= -2
+#Evaluate truthiness of 5 being less than -2
+print "Is it less or equal?", 5 <= -2
